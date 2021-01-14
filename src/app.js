fc27b3202effabf1367131abc7893c9c190f99b1
lizi <530331059@qq.com>
2021-01-14 16:55:20 +0800

    fix:change

--- src/app.js
+++ src/app.js
@@ -3,14 +3,14 @@
  * @Autor: lizi
  * @Date: 2021-01-14 14:02:22
  * @LastEditors: lizi
- * @LastEditTime: 2021-01-14 14:12:13
- * @FilePath: \start-docker\app.js
+ * @LastEditTime: 2021-01-14 16:55:05
+ * @FilePath: \start-docker\src\app.js
  */
 const express = require('express');
 const app = express();
 app.get('/', (req, res) => { 
   res.send('hellow docker');
 })
-app.listen(8081, () => { 
+app.listen(3000, () => { 
   console.log('服务端口开启...')
 })
